16664abebc462c50b3322703d649dabb0e5e6ade
Daniel <daniel.vangioni@devteam.com>
2014-12-12 14:36:52 -0300

    a!

--- www/js/index.js
+++ www/js/index.js
@@ -37,8 +37,12 @@ var app = {
 
         var fbLoginSuccess = function (userData) {
             alert("UserInfo: " + JSON.stringify(userData));
-        }
-
+            facebookConnectPlugin.getAccessToken(function(token) {
+                alert("Token: " + token);
+            }, function(err) {
+                alert("Could not get access token: " + err);
+            });
+        };
         facebookConnectPlugin.login(["public_profile"],
             fbLoginSuccess,
             function (error) { alert("" + error) }
