abede7d50ea7babbd4b952fb8b07d2c34dc5459f
Daniel <daniel.vangioni@devteam.com>
2014-12-12 15:30:01 -0300

    a

---
+++ platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+F:\wamp\www\_github\App-fbConnectTest\platforms\android\com.phonegap.plugins.facebookconnect\FacebookLib\ant-build\AndroidManifest.xml : \
+F:\wamp\www\_github\App-fbConnectTest\platforms\android\com.phonegap.plugins.facebookconnect\FacebookLib\AndroidManifest.xml \
