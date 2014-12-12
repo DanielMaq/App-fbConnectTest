5748063e12a69562f6d301d446e070defd422097
Daniel <daniel.vangioni@devteam.com>
2014-12-12 15:39:25 -0300

    b

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.phonegap.plugins.facebookconnect/www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js",
-        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
-        "clobbers": [
-            "facebookConnectPlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.phonegap.plugins.facebookconnect": "0.10.1"
-}
+{}
 // BOTTOM OF METADATA
 });
--- platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\wamp\www\_github\App-fbConnectTest\platforms\android\com.phonegap.plugins.facebookconnect\FacebookLib\ant-build\AndroidManifest.xml : \
-F:\wamp\www\_github\App-fbConnectTest\platforms\android\com.phonegap.plugins.facebookconnect\FacebookLib\AndroidManifest.xml \
